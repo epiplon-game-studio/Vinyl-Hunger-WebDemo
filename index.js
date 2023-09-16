a27448877dd91f27a514ab34582687285dc3ef05
Vinicius <castvn@proton.me>
2023-09-16 15:40:56 -0300

    cleanup

--- index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-function greeter(person) {
-    return "This is " + person;
-}
-let user = "Vinny";
-document.body.textContent = greeter(user);
--- src/ts/index.ts
+++
@@ -1,7 +0,0 @@
-function greeter(person: string) {
-    return "This is " + person;
-  }
-   
-  let user = "Vinny";
-   
-  document.body.textContent = greeter(user);
